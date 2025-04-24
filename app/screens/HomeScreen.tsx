68f5c72af5352d97c2c9b8cac9adeb246820c66f
Shalabh Sahu <sahushalabh2001@gmail.com>
2025-04-24 15:12:30 +0530

    login done

---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,27 @@
+// app/screens/HomeScreen.tsx
+
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const HomeScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Parth</Text>
+      <Button title="Go to Login" onPress={() => {}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
+import { rmSync } from 'fs';
 
+//handle user registration
 export const registerUser = async (req: Request, res: Response): Promise<any> => {
   console.log('Incoming data:', req.body); // Log the incoming data
 
   const { name, email, password, licensePlate } = req.body;
-
+//check for existing user
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: 'User already exists' });
-
+//create new user
     const user = new User({ name, email, password, licensePlate });
     await user.save();
 
@@ -18,4 +20,20 @@ export const registerUser = async (req: Request, res: Response): Promise<any> =>
     console.error(err); // Log the error if it occurs
     return res.status(500).json({ error: 'Server error' });
   }
+};
+
+//handle user login
+
+export const loginUser = async (req: Request, res: Response): Promise<any> => {
+  const {email, password} = req.body;
+
+  //find user
+  const user = await User.findOne({email});
+  if(!user) {
+    return res.status(400).json({message: 'Invalid email or password'});
+  }
+  if(user.password !== password) {
+    return res.status(400).json({message: 'Invalid email or password'});
+  }
+res.status(200).json({message: 'Login Successful', user});
 };
